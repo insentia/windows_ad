2ad624a9719626e69ac099d48ad830d6144895ec
Jimmy Astle <jimmy@simspace.com>
2015-07-31 15:43:11 -0400

    refactor to remove module complexity

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,13 +12,10 @@
 class windows_ad (
   ### part install AD
   $install                   = 'present',
-  $installmanagementtools    = true,
-  $installsubfeatures        = false,
   $restart                   = false,
   $installflag               = true,                 # Flag to bypass the install of AD if desired
 
   ### Part Configure AD - Global
-  $configure                 = 'present',
   $domain                    = 'forest',
   $domainname                = undef,                # FQDN
   $netbiosdomainname         = undef,                # FQDN
@@ -67,17 +64,7 @@ class windows_ad (
   validate_re($install, '^(present|absent)$', 'valid values for install are \'present\' or \'absent\'')
   # when present configure process will be done. if already configure nothing done
   # absent don't do anything right now
-  validate_re($configure, '^(present|absent)$', 'valid values for configure are \'present\' or \'absent\'')
-  validate_bool($configureflag)
   validate_bool($installflag)
-  
-  class{'windows_ad::install':
-    ensure                 => $install,
-    installmanagementtools => $installmanagementtools,
-    installsubfeatures     => $installsubfeatures,
-    restart                => $restart,
-    installflag            => $installflag,
-  }
 
   class{'windows_ad::conf_forest':
     ensure                    => $configure,
@@ -97,17 +84,12 @@ class windows_ad (
     forceremoval              => $forceremoval,
     uninstalldnsrole          => $uninstalldnsrole,
     demoteoperationmasterrole => $demoteoperationmasterrole,
-    configureflag             => $configureflag,
   }
-  if($installflag or $configureflag){
+  if($installflag ){
     if($install == 'present'){
-      anchor{'windows_ad::begin':} -> Class['windows_ad::install'] -> Class['windows_ad::conf_forest'] -> anchor{'windows_ad::end':} -> Windows_ad::Organisationalunit <| |> -> Windows_ad::Group <| |> -> Windows_ad::User <| |> -> Windows_ad::Groupmembers <| |>
+      anchor{'windows_ad::begin':} -> Class['windows_ad::conf_forest'] -> anchor{'windows_ad::end':} -> Windows_ad::Organisationalunit <| |> -> Windows_ad::Group <| |> -> Windows_ad::User <| |> -> Windows_ad::Groupmembers <| |>
     }else{
-      if($configure == present){
-        fail('You can\'t desactivate the Role ADDS without uninstall ADDSControllerDomain first')
-      }else{
-        anchor{'windows_ad::begin':} -> Class['windows_ad::conf_forest'] -> Class['windows_ad::install'] -> anchor{'windows_ad::end':} 
-      }
+        anchor{'windows_ad::begin':} -> Class['windows_ad::conf_forest'] -> anchor{'windows_ad::end':}
     }
   }else{
     anchor{'windows_ad::begin':} -> Windows_ad::Organisationalunit <| |> -> Windows_ad::Group <| |> -> Windows_ad::User <| |> -> Windows_ad::Groupmembers <| |> -> anchor{'windows_ad::end':}
